19e4f48eef6538cd8a5af9d97b82a670a69ddecb
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-01-18 19:03:43 +0500

    first commit

---
+++ src/shaders/galaxy/fragment.glsl
@@ -0,0 +1,18 @@
+varying vec3 vColor;
+
+void main() {
+    // float strength = 1.0 - step(0.5, distance(gl_PointCoord, vec2(0.5)));
+
+    // Diffuse point
+    // float strength = 1.0 - distance(gl_PointCoord, vec2(0.5)) * 2.0;
+
+    // Light point
+    float strength = pow(1.0 - distance(gl_PointCoord, vec2(0.5)), 10.0);
+
+    // Final color
+    vec3 color = mix(vec3(0.0), vColor, strength);
+
+    gl_FragColor = vec4(color, 1.0);
+
+    #include <colorspace_fragment>
+}
